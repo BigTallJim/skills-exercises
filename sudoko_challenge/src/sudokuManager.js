d9b90d79dd9843368748e74011b1dfe905c247b3
James Holton <james_holton@yahoo.co.uk>
2019-10-11 11:32:34 +0100

    finished with full working version

--- sudoko_challenge/src/sudokuManager.js
+++ sudoko_challenge/src/sudokuManager.js
@@ -1,80 +1,78 @@
 function doneOrNot(board){
-  sedokuManager = new sedokuManager();
-  result = sedokuManager.checkColAndRow(board)
+  
+  result = checkColAndRow(board)
+  
   if (result){
-    result = sedokuManager.checkRegions(board);
+    result = checkRegions(board);
   }
   if (result){
     return 'Finished!'
   }else{
     return 'Try again!'
   }
-}
 
-function sedokuManager() {
-}
-
-sedokuManager.prototype.checkRow = function(row){
-  var rowCheck = {1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false}
-  for (var i=0; i<row.length; i++){
-    rowCheck[row[i]] = true;
-  }
-  return this.validate(rowCheck);
-}
-
-sedokuManager.prototype.validate = function(array){
-  returnValue = true;
-  for (var i=1; i<10; i++){
-    if (!array[i]){
-      returnValue = false;
+  function checkRow(row){
+    var rowCheck = {1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false}
+    for (var i=0; i<row.length; i++){
+      rowCheck[row[i]] = true;
     }
+    return validate(rowCheck);
   }
-  return returnValue;
-}
 
-sedokuManager.prototype.convertColumn = function(array, column){
-  returnArray = [];
-  for (var i=0; i<9; i++){
-    returnArray.push(array[i][column])
-  }
-  return returnArray;
-}
-
-sedokuManager.prototype.checkColAndRow = function(array){
-  returnValue = true;
-  for (var i=0; i<9; i++){
-    if (returnValue){
-      returnValue = this.checkRow(array[i])
-    }
-  }
-  for (var i=0; i<9; i++){
-    if (returnValue){
-      tempRow = this.convertColumn(array, i)
-      returnValue = this.checkRow(tempRow)
-    }
-  }
-  return returnValue;
-}
-
-sedokuManager.prototype.convertRegion = function(array, row, column){
-  returnArray = [];
-  for (var i=0; i<3; i++){
-    for (var j=0; j<3; j++){
-      returnArray.push(array[row+i][column+j])
-    }
-  }
-  return returnArray;
-}
-
-sedokuManager.prototype.checkRegions = function(array){
-  returnValue = true;
-  for (var i=0; i<9; i+=3){
-    for (var j=0; j<9; j+=3){
-      tempRow = this.convertRegion(array,i,j)
-      if (returnValue){
-        returnValue = this.checkRow(tempRow)
+  function validate(array){
+    returnValue = true;
+    for (var i=1; i<10; i++){
+      if (!array[i]){
+        returnValue = false;
       }
     }
+    return returnValue;
   }
-  return returnValue;
+
+  function convertColumn(array, column){
+    returnArray = [];
+    for (var i=0; i<9; i++){
+      returnArray.push(array[i][column])
+    }
+    return returnArray;
+  }
+
+  function checkColAndRow(array){
+    returnValue = true;
+    for (var i=0; i<9; i++){
+      if (returnValue){
+        returnValue = checkRow(array[i])
+      }
+    }
+    for (var i=0; i<9; i++){
+      if (returnValue){
+        tempRow = convertColumn(array, i)
+        returnValue = checkRow(tempRow)
+      }
+    }
+    return returnValue;
+  }
+
+  function convertRegion(array, row, column){
+    returnArray = [];
+    for (var i=0; i<3; i++){
+      for (var j=0; j<3; j++){
+        returnArray.push(array[row+i][column+j])
+      }
+    }
+    return returnArray;
+  }
+
+  function checkRegions(array){
+    returnValue = true;
+    for (var i=0; i<9; i+=3){
+      for (var j=0; j<9; j+=3){
+        tempRow = convertRegion(array,i,j)
+        if (returnValue){
+          returnValue = checkRow(tempRow)
+        }
+      }
+    }
+    return returnValue;
+  }
 }
