4a55d65184f9e3d25ed54bbc3d982ef6107d6d26
James Holton <james_holton@yahoo.co.uk>
2019-10-11 15:57:05 +0100

    create project structure

---
+++ reviewPractice/reviewPractice/spec/reviewPracticeSpec.js
@@ -0,0 +1,12 @@
+describe('reviewPractice', function(){
+  
+  
+  // beforeEach(function() {
+  //   sudokuManager = new doneOrNot(sudokuMap);
+  // });
+
+  // it("single row contains 1-9", function(){
+  //   sudokuRow = [5, 3, 4, 6, 7, 8, 9, 1, 2];
+  //   expect(sudokuManager.checkRow(sudokuRow)).toEqual(true);
+  // });
+});
